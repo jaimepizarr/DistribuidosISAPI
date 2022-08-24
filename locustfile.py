2c2bb633ec043d6e2b11f1e4ca3459a804206a18
jaimepizarr <japibaro@espol.edu.ec>
2022-08-24 10:55:25 -0500

    locust test on users and orders

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,25 @@
-from locust import HttpUser, User, task, between
+from locust import HttpUser, User, task, between, stats
 
-class MyUser(HttpUser):
+stats.CSV_STATS_INTERVAL_SEC = 5
+
+class Docker(HttpUser):
+    host = "http://localhost"
 
     @task
-    def my_task(self):
-        self.client.get("/api/user")
+    def get_users(self):
+        self.client.get("/api/user", name="users-docker")
 
+    @task
+    def get_orders(self):
+        self.client.get("/api/orders", name="orders-docker")
+
+class Pythonanywhere(HttpUser):
+    host = "https://despachomotorizado.pythonanywhere.com/"
+
+    @task
+    def get_users(self):
+        self.client.get("/api/user", name="users-pythonanywhere")
     
+    @task
+    def get_orders(self):
+        self.client.get("/api/orders", name="orders-pythonanywhere")
